3c0ef017e03343d79288de63c411627d53d96718
Blake Eggleston <bdeggleston@gmail.com>
2013-01-27 17:38:19 -0800

    adding simple connection pooling

--- rexpro/connection.py
+++ rexpro/connection.py
@@ -2,6 +2,7 @@ __author__ = 'bdeggleston'
 
 from contextlib import contextmanager
 from hashlib import md5
+from Queue import Queue
 from socket import socket
 from textwrap import dedent
 
@@ -9,13 +10,112 @@ from rexpro import exceptions
 from rexpro import messages
 from rexpro import utils
 
+class RexProSocket(socket):
+    """ Subclass of python's socket that sends and received rexpro messages """
+
+    def send_message(self, msg):
+        """
+        Serializes the given message and sends it to rexster
+
+        :param msg: the message instance to send to rexster
+        :type msg: RexProMessage
+        """
+        self.send(msg.serialize())
+
+    def get_response(self):
+        """
+        gets the message type and message from rexster
+
+        :returns: RexProMessage
+        """
+        msg_type = self.recv(1)
+        if not msg_type:
+            raise exceptions.RexProConnectionException('socket connection has been closed')
+        msg_type = bytearray(msg_type)[0]
+        msg_len = utils.int_from_32bit_array(self.recv(4))
+        response = self.recv(msg_len)
+
+        MessageTypes = messages.MessageTypes
+
+        type_map = {
+            MessageTypes.ERROR: messages.ErrorResponse,
+            MessageTypes.SESSION_RESPONSE: messages.SessionResponse,
+            MessageTypes.MSGPACK_SCRIPT_RESPONSE: messages.MsgPackScriptResponse
+        }
+
+        if msg_type not in type_map:
+            raise exceptions.RexProConnectionException("can't deserialize message type {}".format(msg_type))
+        return type_map[msg_type].deserialize(response)
+
+class RexProConnectionPool(object):
+
+    def __init__(self, host, port, size):
+        """
+        Connection constructor
+
+        :param host: the server to connect to
+        :type host: str (ip address)
+        :param port: the server port to connect to
+        :type port: int
+        :param size: the initial connection pool size
+        :type size: int
+        """
+
+        self.host = host
+        self.port = port
+        self.size = size
+
+        self.pool = Queue()
+        for i in range(size):
+            self.pool.put(self._new_conn())
+
+    def _new_conn(self):
+        """
+        Creates and returns a new connection
+        """
+        conn = RexProSocket()
+        conn.connect((self.host, self.port))
+        return conn
+
+    def get(self):
+        """
+        Returns a connection, creating a new one if the pool is empty
+        """
+        if self.pool.empty():
+            return self._new_conn()
+        return self.pool.get()
+
+    def put(self, conn):
+        """
+        returns a connection to the pool, will close the connection if the pool is full
+        """
+        if self.pool.qsize() >= self.size:
+            conn.close()
+            return
+
+        self.pool.put(conn)
+
+    @contextmanager
+    def contextual_connection(self):
+        """
+        context manager that will open, yield, and close a connection
+        """
+        conn = self.get()
+        yield conn
+        self.put(conn)
+
+    def __del__(self):
+        while not self.pool.empty():
+            self.pool.get().close()
+
+
 class RexProConnection(object):
 
     #determines which format rexster returns data in
     # 1 is a string format for consoles
     # 2 is a msgpack format, which we want
     CHANNEL = 2
-    def __init__(self, host, port, graph_name, username='', password=''):
+    def __init__(self, host, port, graph_name, username='', password='', poolsize=4):
         """
         Connection constructor
 
@@ -29,16 +129,18 @@ class RexProConnection(object):
         :type username: str
         :param password: the password to use for authentication (optional)
         :type password: str
+        :param poolsize: the initial connection pool size
+        :type poolsize: int
         """
         self.host = host
         self.port = port
         self.graph_name = graph_name
         self.username = username
         self.password = password
+        self.poolsize = poolsize
 
         #connect to server
-        self._socket = socket()
-        self._socket.connect((host, port))
+        self._pool = RexProConnectionPool(self.host, self.port, self.poolsize)
 
         #indicates that we're in a transaction
         self._in_transaction = False
@@ -50,15 +152,16 @@ class RexProConnection(object):
 
     def _open_session(self):
         """ Creates a session with rexster and creates the graph object """
-        self._send_message(
-            messages.SessionRequest(
-                channel=self.CHANNEL,
-                username=self.username,
-                password=self.password
+        with self._pool.contextual_connection() as conn:
+            conn.send_message(
+                messages.SessionRequest(
+                    channel=self.CHANNEL,
+                    username=self.username,
+                    password=self.password
+                )
             )
-        )
-        session = self._get_response()
-        self._session_key = session.session_key
+            session = conn.get_response()
+            self._session_key = session.session_key
 
         results = self.execute(
             script='g = rexster.getGraph(graphname)',
@@ -68,40 +171,6 @@ class RexProConnection(object):
         if not results:
             raise exceptions.RexProConnectionException("could not connect to graph '{}'".format(self.graph_name))
 
-
-    def _send_message(self, msg):
-        """
-        Serializes the given message and sends it to rexster
-
-        :param msg: the message instance to send to rexster
-        :type msg: RexProMessage
-        """
-        self._socket.send(msg.serialize())
-
-    def _get_response(self):
-        """
-        gets the message type and message from rexster
-
-        :returns: RexProMessage
-        """
-        msg_type = self._socket.recv(1)
-        if not msg_type:
-            raise exceptions.RexProConnectionException('socket connection has been closed')
-        msg_type = bytearray(msg_type)[0]
-        msg_len = utils.int_from_32bit_array(self._socket.recv(4))
-        response = self._socket.recv(msg_len)
-
-        MessageTypes = messages.MessageTypes
-
-        type_map = {
-            MessageTypes.ERROR: messages.ErrorResponse,
-            MessageTypes.SESSION_RESPONSE: messages.SessionResponse,
-            MessageTypes.MSGPACK_SCRIPT_RESPONSE: messages.MsgPackScriptResponse
-        }
-
-        if msg_type not in type_map:
-            raise exceptions.RexProConnectionException("can't deserialize message type {}".format(msg_type))
-        return type_map[msg_type].deserialize(response)
 
     def open_transaction(self):
         """ opens a transaction """
@@ -153,24 +222,26 @@ class RexProConnection(object):
 
         :rtype: list
         """
-        query_script = dedent(script) if pretty else script
-        if isolate:
-            closure_name = 'q_{}'.format(md5(query_script).hexdigest())
-            query_script = '\n'.join([
-                'def %s = {' % closure_name,
-                query_script,
-                '}',
-                '%s()' % closure_name
-            ])
+        with self._pool.contextual_connection() as conn:
+            query_script = dedent(script) if pretty else script
+            if isolate:
+                closure_name = 'q_{}'.format(md5(query_script).hexdigest())
+                query_script = '\n'.join([
+                    'def %s = {' % closure_name,
+                    query_script,
+                    '}',
+                    '%s()' % closure_name
+                ])
 
-        self._send_message(
-            messages.ScriptRequest(
-                script=query_script,
-                params=params,
-                session_key=self._session_key
+            conn.send_message(
+                messages.ScriptRequest(
+                    script=query_script,
+                    params=params,
+                    session_key=self._session_key
+                )
             )
-        )
-        response = self._get_response()
+            response = conn.get_response()
+
         if isinstance(response, messages.ErrorResponse):
             raise exceptions.RexProScriptException(response.message)
 
