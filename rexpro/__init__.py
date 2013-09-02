6879c2d5a8d01b5f40847befdcd534295cef24d5
Blake Eggleston <bdeggleston@gmail.com>
2013-09-02 12:54:31 -0700

    version bump / removing alpha status

--- rexpro/__init__.py
+++ rexpro/__init__.py
@@ -1,4 +1,4 @@
 __author__ = 'bdeggleston'
-__version__ = "0.1"
+__version__ = "0.1.1"
 
 from rexpro.connection import RexProConnection
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 #python setup.py register
 #python setup.py sdist upload
 
-version = "0.1"
+version = "0.1.1"
 
 long_desc = """
 Experimental Python RexPro interface
@@ -17,7 +17,6 @@ setup(
     dependency_links=['https://github.com/bdeggleston/rexpro-python/archive/{0}.tar.gz#egg=rexpro-python-{0}'.format(version)],
     long_description=long_desc,
     classifiers=[
-        "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.6",
